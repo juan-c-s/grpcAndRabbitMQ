7494ade289d327209341225993f4d204e6d32e8a
juan-c-s <juancamilo.salazar2002@gmail.com>
2023-08-30 17:07:26 -0500

    Final

--- apigw/index.js
+++ apigw/index.js
@@ -17,22 +17,6 @@ app.post('/listFiles', async (req, res) => {
     }
   res.status(201).send('success');
 });
-// app.get('/listFiles', async (req, res) => {
-//     try{
-//         const obj = {
-//             method : "listFiles"
-//         }
-//         await publishMessage(JSON.stringify(obj))
-
-//     }catch(e){
-//         const obj = {
-//             method : "listFiles"
-//         }
-//         await publishMessage(JSON.stringify(obj))
-//         console.log("Error Found: ",e);
-//     }
-//   res.status(201).send('success');
-// });
 
 app.post('/findFile', async (req, res) => {
     const body = req.body;
@@ -44,34 +28,6 @@ app.post('/findFile', async (req, res) => {
     }
   res.status(201).send('success');
 });
-// app.post('/findFile', async (req, res) => {
-//     const body = req.body;
-//     console.log( body.fileName);
-//     try{
-
-//         const obj = {
-//             method : "findFile",
-//             fileName : body.fileName
-//         }
-//         console.log(obj);
-//         console.log(JSON.stringify(obj));
-//         await publishMessage(JSON.stringify(obj))
-
-//     }catch(e){
-//         console.log("Error Found: ",e);
-//         const obj = {
-//             method : "findFile",
-//             fileName : body.filename
-//         }
-       
-//         await publishMessage(JSON.stringify(obj))
-
-//     }
-//   res.status(201).send('success');
-// });
-
-
-
 
 const port = 80;
 app.listen(port, () => console.log('Running on port ', port));
